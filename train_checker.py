ddef6cb7885f46340686efcf0059d3d9a5fcfd2b
oleksiilatypov <latypov.oleksii.la@gmail.com>
2024-01-28 23:41:43 +0200

    rename_files

--- train_checker.py
+++ train_checker.py
@@ -1,34 +1,9 @@
 from sklearn.linear_model import LogisticRegression
 
-from settings.constants import TRAIN_CSV
-from utils.dataloader import DataLoader
-import pandas as pd
-
-# train = pd.read_csv(TRAIN_CSV)
-#
-# #X_raw = train.drop("RainTomorrow", axis=1)
-#
-# #print(X_raw.head())
-# print(100*'>')
-#
-# loader = DataLoader()
-# loader.fit(train)
-# X_raw = loader.load_data()
-#
-# print(100*'=')
-# X = X_raw.drop('RainTomorrow', axis=1)
-# print(X.head())
-#
-# print(100 * '==')
-# y = X_raw["RainTomorrow"]
-# print(y.head())
-#
-# print(X.head())
 
 import pickle
 import json
 import pandas as pd
-from sklearn.svm import SVC
 
 from utils.dataloader import DataLoader
 from settings.constants import TRAIN_CSV
